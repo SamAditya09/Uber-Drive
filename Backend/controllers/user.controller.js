5b0b94942321855a9cf827c9649d7c733aef5ac5
Aditya Kumar Samantaray <adityasamantaray985@gmail.com>
2024-12-07 14:03:56 +0530

    user login done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -10,6 +10,12 @@ module.exports.registerUer = async (req, res, next) => {
 
   const { fullName, email, password } = req.body;
 
+  const existingUser = await userModel.findOne({ email });
+
+  if (existingUser) {
+    return res.status(409).json({ message: "User already exists" });
+  }
+
   const hashPassword = await userModel.hashPassword(password);
   const user = await userService.createUser({
     firstName: fullName.firstName,
@@ -22,3 +28,28 @@ module.exports.registerUer = async (req, res, next) => {
 
   res.status(201).json({ user, token });
 };
+
+module.exports.loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email }).select("+password");
+
+  if (!user) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  const isMatch = await user.comparePassword(password);
+
+  if(!isMatch){
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  const token = user.generateAuthToken();
+
+  res.status(200).json({ user, token });
+}
