33ec264a1f0c665221438993aae32c47622f47bc
Aditya Kumar Samantaray <adityasamantaray985@gmail.com>
2024-12-10 12:54:10 +0530

    captain login profile logout done

--- Backend/models/blacklistToken.model.js
+++ Backend/models/blacklistToken.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const blacklistTokenSchema = new mongoose.Schema({
   token: { type: String, required: true, unique: true },
-  createdAt: { type: Date, default: Date.now, index: { expires: 86400 } },
+  createdAt: { type: Date, default: Date.now, expires: 86400 },
 });
 
 module.exports = mongoose.model("blacklistToken", blacklistTokenSchema);
--- Backend/routes/captain.route.js
+++ Backend/routes/captain.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {body} = require('express-validator');
 const captainController = require('../controllers/captain.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/register',[
     body("fullName.firstName").isLength({min:3}).withMessage("Name must be at least 3 characters long"),
@@ -13,5 +14,14 @@ router.post('/register',[
     body("vehicle.vehicleType").isIn(['car', 'motorcycle', 'auto']).withMessage("vehicleType must be 'car', 'motorcycle', or 'auto'"),
 ],captainController.registerCaptain);
 
+router.post('/login',[
+    body("email").isEmail().withMessage("Invalid email address"),
+    body("password").isLength({min:6}).withMessage("Password must be at least 6 characters long"),
+]
+,captainController.loginCaptain);
+
+router.get('/profile',authMiddleware.authCaptain,captainController.getCaptainProfile);
+
+router.post('/logout',captainController.logoutCaptain);
 
 module.exports = router;
