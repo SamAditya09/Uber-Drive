d3a31ff7d5037a393dced581b169879cc776e62b
Aditya Kumar Samantaray <adityasamantaray985@gmail.com>
2024-12-28 21:05:25 +0530

    OTP function added

--- Backend/services/ride.service.js
+++ Backend/services/ride.service.js
@@ -1,5 +1,6 @@
 const rideModel = require("../models/ride.model");
 const mapService = require("./map.service");
+const crypto = require("crypto");
 
 async function getFare(pickup, destination) {
     if(!pickup || !destination) {   
@@ -51,6 +52,14 @@ async function getFare(pickup, destination) {
     return fare;
 }
 
+function getOtp(num) {
+    function generateOtp(num) {
+        const otp = crypto.randomInt(Math.pow(10, num - 1), Math.pow(10, num)).toString();
+        return otp;
+    }
+    return generateOtp(num);
+}
+
 module.exports.createRide = async ({
     user,pickup,destination,vehicleType 
 }) => {
@@ -69,6 +78,7 @@ module.exports.createRide = async ({
             user,
             pickup,
             destination,
+            otp: getOtp(6),
             fare: fare[vehicleType],
             vehicleType,
         });
